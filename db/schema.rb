27155d205586d324c125144ccb4473aa32d8a7f6
idgm5 <idgm@pm.me>
2020-09-07 16:26:07 -0500

    Switch sqlite db for PG db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_03_002013) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "appointments", force: :cascade do |t|
     t.string "model"
     t.string "city"
