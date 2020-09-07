b17aab56acb39141078dae8cbb8bba54328cb996
idgm5 <idgm@pm.me>
2020-09-07 17:24:59 -0500

    Limit endpoints for controllers in routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :bikes
-  resources :appointments
+  resources :bikes, :only => [:index]
+  resources :appointments, :only => [:index, :create]
 end
