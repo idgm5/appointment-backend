a01295549a0fe0de3c6af25089c0216df0fbe6ba
idgm5 <idgm@pm.me>
2020-09-08 10:43:13 -0500

    Fix factories linters

--- spec/factories/bike.rb
+++ spec/factories/bike.rb
@@ -5,7 +5,10 @@ FactoryBot.define do
     option { Faker::Number.between(from: 0, to: 10) }
     total { Faker::Number.between(from: 0, to: 10) }
     duration { Faker::Number.between(from: 0, to: 10) }
-    description { Faker::Lorem.sentence(word_count: word_count = 4, supplemental: supplemental = false, random_words_to_add: random_words_to_add = 0)}
+    description {
+      Faker::Lorem.sentence(word_count: word_count = 4,
+                            supplemental: supplemental = false, random_words_to_add: random_words_to_add = 0)
+    }
     picture { Faker::Name.name }
   end
 end
