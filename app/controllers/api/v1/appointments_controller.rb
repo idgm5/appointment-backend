f752c2b8ccf1544bc2508eacff8b2fd526d75251
idgm5 <idgm@pm.me>
2020-09-08 11:05:47 -0500

    fix assc for heroku

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -10,10 +10,10 @@ class Api::V1::AppointmentsController < ApiController
   end
 
   def create
-    @bike_id = Bike.find_by(modelName: params[:model])
+    @bike = Bike.find_by(modelName: params[:model])
     @appointment = Appointment.create(user: params[:user], model: params[:model],
                                       city: params[:city], date: params[:date],
-                                      bike_id: @bike_id)
+                                      bike_id: @bike.id)
     render json: @appointment
   end
 
